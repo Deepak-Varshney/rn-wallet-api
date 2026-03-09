df0854b5672a88e8bed4816caa9c95561b4230ad
Deepak Varshney <deepakvarshney.com@gmail.com>
2026-03-10 03:13:18 +0530

    Fix corss error

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { sql } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json())
+app.use(cors({
+    origin: "*" // Allow all origins
+}))
 const PORT = process.env.PORT || 5001
 
 
